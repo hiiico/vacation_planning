e321a6d0b70b9408faa70617d59811ea429ffa07
hiiico <hiiico332@gmail.com>
2025-03-23 21:14:13 +0200

    Commit for this project

---
+++ src/main/java/app/user/service/UserInit.java
@@ -0,0 +1,33 @@
+package app.user.service;
+
+import app.user.model.Country;
+import app.web.dto.RegisterRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserInit implements CommandLineRunner {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserInit(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        if (!userService.getAllUser().isEmpty()) {
+            return;
+        }
+        RegisterRequest registerRequest = RegisterRequest.builder()
+                .username("H.Ivanov")
+                .password("Hristo1972")
+                .country(Country.GERMANY)
+                .build();
+        userService.register(registerRequest);
+    }
+
+}
---
+++ src/main/java/app/user/service/UserService.java
@@ -0,0 +1,129 @@
+package app.user.service;
+
+import app.exception.DomainException;
+import app.notification.service.NotificationService;
+import app.security.AuthenticationDetails;
+import app.user.model.User;
+import app.user.model.UserRole;
+import app.user.repository.UserRepository;
+import app.web.dto.RegisterRequest;
+import app.web.dto.UserEditRequest;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final NotificationService notificationService;
+
+@Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, NotificationService notificationService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.notificationService = notificationService;
+    }
+
+
+    @CacheEvict(value = "users", allEntries = true)
+    @Transactional
+    public User register(RegisterRequest registerRequest) {
+
+        Optional<User> userOptional = userRepository.findByUsername(registerRequest.getUsername());
+
+        if(userOptional.isPresent()) {
+            throw new DomainException("Username [%s] already exist.".formatted(registerRequest.getUsername()));
+        }
+
+        User user = userRepository.save(initializeUser(registerRequest));
+
+     //   notificationService.saveNotificationPreference(user.getId(), false, null);
+
+        log.info("Successfully created new user account for username [%s] and id [%s]"
+                .formatted(registerRequest.getUsername(), user.getId()));
+        return user;
+    }
+
+    @CacheEvict(value = "users", allEntries = true)
+    public void editUserDetails(UUID userId, UserEditRequest userEditRequest) {
+
+        User user = getById(userId);
+        user.setFirstName(userEditRequest.getFirstName());
+        user.setLastName(userEditRequest.getLastName());
+        user.setEmail(userEditRequest.getEmail());
+        user.setProfilePicture(userEditRequest.getProfilePicture());
+
+        userRepository.save(user);
+    }
+
+    private User initializeUser(RegisterRequest registerRequest) {
+        return User.builder()
+                .username(registerRequest.getUsername())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .role(UserRole.USER)
+                .isActive(true)
+                .country(registerRequest.getCountry())
+                .createdOn(LocalDateTime.now())
+                .updatedOn(LocalDateTime.now())
+                .build();
+    }
+
+@Cacheable("users")
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+
+    public User getById(UUID id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new DomainException("User with id [%s] does not exist."));
+    }
+@CacheEvict(value = "users", allEntries = true)
+    public void switchStatus(UUID id) {
+
+        User user = getById(id);
+        user.setActive(!user.isActive());
+        userRepository.save(user);
+    }
+
+    @CacheEvict(value = "users", allEntries = true)
+    public void switchRole(UUID id) {
+
+        User user = getById(id);
+        if(user.getRole() == UserRole.USER) {
+            user.setRole(UserRole.ADMIN);
+        } else {
+            user.setRole(UserRole.USER);
+        }
+
+        userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+       User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new DomainException("User with this username does not exist"));
+
+       return new AuthenticationDetails(
+                user.getId(),
+                user.getUsername(),
+                user.getPassword(),
+                user.getRole(),
+                user.isActive());
+    }
+}
