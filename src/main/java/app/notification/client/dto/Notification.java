65890303779832094c9bc9ca04b60ecf9196e816
hiiico <hiiico332@gmail.com>
2025-03-26 22:39:06 +0200

    Commit for this project

---
+++ src/main/java/app/notification/client/dto/Notification.java
@@ -0,0 +1,18 @@
+package app.notification.client.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Notification {
+
+    private String subject;
+
+    private LocalDateTime createdOn;
+
+    private String status;
+
+    private String type;
+
+}
